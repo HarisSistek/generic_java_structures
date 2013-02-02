590077b010ee4b73a09cf02c3c3a132d27eb5aa9
HarisSistek <ha.sistek@gmail.com>
2013-02-02 08:45:31 +0100

    Added remove and size to the list

--- LinkedList.java
+++ LinkedList.java
@@ -3,16 +3,18 @@
 class LinkedList <E extends Comparable <E>> {
     
     private Node<E> head;
+    private int size;
     
     LinkedList () {
-	
+	size = 0;
     }
     
     /**
      *Adds at the end of the list, no sorting 
      */
     public void add(E e) {
-	
+	size++;
+
 	if (head == null) 
 	    head = new Node<E> (e);
 	
@@ -31,6 +33,27 @@ class LinkedList <E extends Comparable <E>> {
 	}
 
 	return head.getElementFromList(i);
+    }
+
+    /**
+     *Remove the node e in the list
+     *the node will do most of the work
+     */
+    public void remove(E e) {
+	if (head == null) 
+	    return;
+
+	if (size == 1) {
+	    size--;
+	    head = null;
+	    return;
+	}
+	size--;
+	head.remove(e);
+    }
+
+    public int size() {
+	return size;
     }
     
 }
